dc530d06e90d2a570209632420ac9f8ce7aceae7
Saeed Rasooli <saeed.gnu@gmail.com>
2016-05-31 20:38:43 +0430

    add __init__, move and getPos to Unit class

--- chess.py
+++ chess.py
@@ -3,6 +3,30 @@ class Unit(object):
     name = ''
     moveSteps = ()
     moveMaxLength = 1
+    def __init__(self, col, row):
+        """
+            col: column index, starting from 0
+            row: row index, starting from 0
+        """
+        self.move(col, row)
+    
+    def move(self, col, row):
+        """
+            col: column index, starting from 0
+            row: row index, starting from 0
+            
+            moves the unit to the position
+        """
+        self.col = col
+        self.row = row
+
+    def getPos(self):
+        """
+            returns the current position as tuple (col, row)
+        """
+        return (self.col, self.row)
+
+
 
 
 class King(Unit):
