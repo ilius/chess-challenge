e94354a970ac11611b492ce30e4942fd2ecbf77a
Saeed Rasooli <saeed.gnu@gmail.com>
2016-06-01 09:18:01 +0430

    findSolutions: add cellNum to stack item to avoid duplicate solutions

--- solution.py
+++ solution.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from units import Unit
 
+
 def findSolutions(rowCount, colCount, symbolsCount):
     """
         rowCount: int, number or rows
@@ -10,10 +11,16 @@ def findSolutions(rowCount, colCount, symbolsCount):
         this is a generator, yields a completed `board` each time
         where `board` is a dict of {(rowNum, colNum) => unitSymbol}
     """
-    ## `todo` is a stack (we use .append, and .pop), each item is a tuple of (board, stage)
-    ## where `board` is a dict of {(rowNum, colNum) => unitSymbol}
-    ## and `stage` is a tuple containing count or each unit type
-    ## stage == (kingCount, queenCount, bishopCount, rookCount, knightCount)
+    # `todo` is a stack (we use .append, and .pop), each item is a tuple of (board, stage, cellNum)
+    #   `board` is a dict of {(rowNum, colNum) => unitSymbol}
+    #   `stage` is a tuple containing count or each unit type
+    #       stage == (kingCount, queenCount, bishopCount, rookCount, knightCount)
+    #   `cellNum` is an int resulting from `rowNum * colCount + colNum`
+    #       representing the next cell that we want to check
+    #       and we don't want any unit to put before this cell on the current board
+    #       this way we avoid giving duplicate solutions and extra computation
+    #       To decode cellNum: rowNum, colNum = divmod(cellNum, colCount)
+    cellCount = rowCount * colCount
     stage = (
         symbolsCount.get(cls.symbol, 0)
         for cls in Unit.classList
@@ -21,37 +28,45 @@ def findSolutions(rowCount, colCount, symbolsCount):
     todo = [(
         {}, ## initial board
         stage,## initial stage
+        0,## first cell (top-left corner)
     )]
     
     while todo:## stack not empty
-        board, stage = todo.pop()
-        
-        for unitId, count in enumerate(stage):
-            if not count:
-                continue
-            unitCls = Unit.classList[unitId]
-            for rowNum in range(rowCount):
-                for colNum in range(colCount):
-                    if (rowNum, colNum) in board:
-                        continue
-                    unit = unitCls(rowNum, colNum)
-                    if not unit.canPutOnBoard(board):
-                        continue
-                    
-                    newBoard = board.copy()
-                    newBoard[(rowNum, colNum)] = unit.symbol
-                    
-                    newStage = list(stage)
-                    newStage[unitId] -= 1
-                    assert newStage[unitId] >= 0
+        board, stage, startCellNum = todo.pop()
 
-                    if not any(newStage):## newStage is empty, newBoard is complete
-                        yield newBoard
+        for cellNum in range(startCellNum, cellCount):
+            (rowNum, colNum) = cellPos = divmod(cellNum, colCount)
+            for unitId, count in enumerate(stage):
+                if not count:
+                    continue
+                unitCls = Unit.classList[unitId]
+                if cellPos in board:
+                    continue
+                unit = unitCls(rowNum, colNum)
+                if not unit.canPutOnBoard(board):
+                    continue
 
-                    
-                    todo.append((newBoard, tuple(newStage)))
+                newBoard = board.copy()
+                newBoard[cellPos] = unit.symbol
+
+                newStage = list(stage)
+                newStage[unitId] -= 1
+                assert newStage[unitId] >= 0
+
+                if not any(newStage):## newStage is empty, newBoard is complete
+                    yield newBoard
+
+                if cellNum < cellCount - 1:
+                    todo.append((
+                        newBoard,
+                        tuple(newStage),
+                        cellNum + 1,
+                    ))
             
     
     
+
+
+
 
 
