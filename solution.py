eed846e20651c43cf9b61bbb98ee4db044e16afe
Saeed Rasooli <saeed.gnu@gmail.com>
2016-06-02 01:19:15 +0430

    add queue.Queue implementation of findSolutions, looks like twice slower than Stack and Recursive implementations

--- solution.py
+++ solution.py
@@ -158,4 +158,84 @@ def findSolutions_R(rowCount, colCount, countBySymbol):
     )
 
 
+def findSolutions_Q(rowCount, colCount, countBySymbol, debug=False):
+    """find and iterate over solution boards, implemented with Queue
+    
+    rowCount: int, number or rows
+    colCount: int, number of columns
+    countBySymbol: dict of { unitSymbol => count }
+
+    this is a generator, yields a completed `board` each time
+    where `board` is a dict of {(rowNum, colNum) => unitSymbol}
+    """
+    from queue import Queue
+    # `todo` is a Queue instance (we use .empty, .put, and .get)
+    # each item is a tuple of (board, stage, cellNum)
+    #   `board` is a dict of {(rowNum, colNum) => unitSymbol}
+    #   `stage` is a tuple containing count or each unit type:
+    #       (kingCount, queenCount, bishopCount, rookCount, knightCount)
+    #   `cellNum` is an int resulting from `rowNum * colCount + colNum`
+    #       representing the next cell that we want to check
+    #       and we don't want any unit to put before this cell on the current
+    #       board, this way we avoid giving duplicate solutions and extra
+    #       computation
+    #       To decode cellNum: rowNum, colNum = divmod(cellNum, colCount)
+    cellCount = rowCount * colCount
+    stage = tuple(
+        countBySymbol.get(cls.symbol, 0)
+        for cls in Unit.classList
+    )
+    todo = Queue()
+    todo.put((
+        {},     # initial board
+        stage,  # initial stage
+        0,      # first cell (top-left corner)
+    ))
+
+    while not todo.empty():  # queue not empty
+        board, stage, cellNum = todo.get()
+        stageSize = sum(stage)
+
+        if debug:
+            print('len(todo)=%s, len(board)=%s, stage=%s, cellNum=%s'%(todo.qsize(), len(board), str(stage), cellNum))
+
+        if cellNum < cellCount - stageSize:
+            # we can leave cell empty, skip to next one
+            todo.put((
+                board,
+                stage,
+                cellNum + 1,
+            ))
+
+        (rowNum, colNum) = cellPos = divmod(cellNum, colCount)
+        for unitId, count in enumerate(stage):
+            if count < 1:
+                continue
+            unitCls = Unit.classList[unitId]
+            if cellPos in board:
+                continue
+            unit = unitCls(rowNum, colNum)
+            if not unit.canPutOnBoard(board):
+                continue
+
+            newBoard = board.copy()
+            newBoard[cellPos] = unit.symbol
+
+            newStage = list(stage)
+            newStage[unitId] -= 1
+            assert newStage[unitId] >= 0
+
+            if stageSize <= 1:  # newStage empty, newBoard complete
+                yield newBoard
+                continue
+
+            if cellNum < cellCount - (stageSize - 1):
+                todo.put((
+                    newBoard,
+                    tuple(newStage),
+                    cellNum + 1,
+                ))
+
+
+
 findSolutions = findSolutions_S
