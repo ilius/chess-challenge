f804cf19990805585df5c5cde835368b3a0d902f
Saeed Rasooli <saeed.gnu@gmail.com>
2016-06-02 11:03:17 +0430

    pylint score: main.py: 10.0/10

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 #!/usr/bin/env python3
+"""responsible for calling other modules and interacting with user"""
 
 import sys
+from time import time as now
+import random
 
 from units import Unit
-from solution import find_solutions_s as find_solutions
+from solution import (
+    find_solutions_s,
+    find_solutions_r,
+    find_solutions_q,
+)
 
 
 def input_int(prompt, default=None, minimum=None, maximum=None):
+    """
+    ask the user to enter an integer number
+    make sure it's an integer, and within possibly given criteria
+    default: default value (if the user leaves it empty)
+             if default is None, user can not leave it empty
+    minimum: minimum allowed value, or None
+    maximum: maximum allowed value, or None
+    """
     while True:
         value_str = input(prompt).strip()
         if not value_str:
@@ -33,6 +48,13 @@ def input_int(prompt, default=None, minimum=None, maximum=None):
 
 
 def input_units_count(row_count, col_count):
+    """
+    ask the user the number or chess units / pieces of each type
+    return a dict { unit_symbol => count }
+
+    row_count: number of rows
+    col_count: number of columns
+    """
     cell_count = row_count * col_count
     count_by_symbol = {}
     total_count = 0
@@ -50,6 +72,13 @@ def input_units_count(row_count, col_count):
 
 
 def format_board(board, row_count, col_count):
+    """
+    convert a `board` into string than can be shown in console
+
+    board: a dict { (row_num, col_num) => unit_symbol }
+    row_count: number of rows
+    col_count: number of columns
+    """
     sep_line = '-' * (col_count * 4 + 1)
     lines = [sep_line]
     for row_num in range(row_count):
@@ -64,7 +93,7 @@ def format_board(board, row_count, col_count):
 
 
 def make_random_board(row_count, col_count, density=0.5):
-    import random
+    """create a random chess board with given size and density"""
     board = {}
     for row_num in range(row_count):
         for col_num in range(col_count):
@@ -77,6 +106,9 @@ def make_random_board(row_count, col_count, density=0.5):
 
 
 def input_problem():
+    """
+    get the board size and units count from stdin or command line arguments
+    """
     if len(sys.argv) == 3 + len(Unit.class_list):
         row_count = int(sys.argv[1])
         col_count = int(sys.argv[2])
@@ -100,14 +132,19 @@ def input_problem():
 
 
 def main():
+    """
+    ask the board size and units count
+    calculate and show all possible unique configuration
+    """
     row_count, col_count, count_by_symbol = input_problem()
     print('Found Configurations:\n')
-    for board in find_solutions(row_count, col_count, count_by_symbol):
+    for board in find_solutions_s(row_count, col_count, count_by_symbol):
         print(format_board(board, row_count, col_count))
         input('Press enter to see the next')
 
 
 def test_input_int():
+    """test `input_int` function"""
     print(input_int('Enter an integer: '))
     print(input_int('Enter an integer (default=0): ', default=0))
     print(input_int('Enter an integer (>= 3): ', minimum=3))
@@ -121,6 +158,7 @@ def test_input_int():
 
 
 def test_input_units_count(row_count, col_count):
+    """test `input_units_count` function"""
     count_by_symbol = input_units_count(row_count, col_count)
     assert set(count_by_symbol.keys()) == set(Unit.class_by_symbol.keys())
     for count in count_by_symbol.values():
@@ -134,6 +172,7 @@ def test_input_units_count(row_count, col_count):
 
 
 def test_format_random_board(density=0.5):
+    """test `format_random_board` function"""
     while True:
         row_count = input_int('Number of rows: ', minimum=2, default=0)
         if row_count == 0:
@@ -145,16 +184,23 @@ def test_format_random_board(density=0.5):
 
 
 def compare_find_solutions_result():
-    from solution import find_solutions_s, find_solutions_r, find_solutions_q
+    """
+    run and compare the result of 3 implementations of find_solutions
+    make sure they all return the same set of configurations
+    with no duplicates
+    """
     row_count, col_count, count_by_symbol = input_problem()
 
     solution_set_list = []
     # solution_set_list is a list of sets, one set for each implementation
-    for func in (
+
+    func_list = (
         find_solutions_r,
         find_solutions_q,
         find_solutions_s,
-    ):
+    )
+
+    for func in func_list:#  pylint!
         solution_set = set()
         for board in func(row_count, col_count, count_by_symbol):
             board_tuple = tuple(sorted(board.items()))
@@ -167,25 +213,28 @@ def compare_find_solutions_result():
 
 
 def compare_find_solutions_time():
-    from time import time
-    from solution import find_solutions_s, find_solutions_r, find_solutions_q
+    """
+    run and compare the running time of 3 implementations of find_solutions
+    """
 
     row_count, col_count, count_by_symbol = input_problem()
 
     time_list = []
 
-    for func in (
+    func_list = (
         find_solutions_s,
         find_solutions_r,
         find_solutions_q,
         find_solutions_s,
         find_solutions_r,
         find_solutions_q,
-    ):
-        tm0 = time()
-        for board in func(row_count, col_count, count_by_symbol):
+    )
+
+    for func in func_list:#  pylint!
+        tm0 = now()
+        for _ in func(row_count, col_count, count_by_symbol):
             pass
-        delta = time() - tm0
+        delta = now() - tm0
         time_list.append(delta)
         print('%.4f seconds   (%s)' % (delta, func))
 
