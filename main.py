f73828b17669614e3026be767e481b7fc6001ea8
Saeed Rasooli <saeed.gnu@gmail.com>
2016-06-07 14:58:49 +0430

    main.py: move some functions to new file cmd_chess_util.py

--- main.py
+++ main.py
@@ -17,62 +17,7 @@ from solution import (
 )
 from chess_util import format_board
 from cmd_util import input_yesno
-
-PIECE_SYMBOLS = 'KQBRN'  # must not re-order
-
-
-def input_pieces_count(row_count, col_count):
-    """
-    ask the user the number or chess pieces of each type
-    return a dict { piece_symbol => count }
-
-    row_count: number of rows
-    col_count: number of columns
-    """
-    cell_count = row_count * col_count
-    count_by_symbol = {}
-    total_count = 0
-    for symbol in PIECE_SYMBOLS:
-        cls = ChessPiece.class_by_symbol[symbol]
-        plural_name = cls.name.capitalize() + 's'
-        count = input_int(
-            'Number of %s: ' % plural_name,
-            default=0,
-            minimum=0,
-            maximum=cell_count-total_count,
-        )
-        count_by_symbol[symbol] = count
-        total_count += count
-    return count_by_symbol
-
-
-def input_problem():
-    """
-    get the board size and pieces count from stdin
-    """
-    row_count = input_int('Number of rows: ', minimum=2)
-    col_count = input_int('Number of columns: ', minimum=2)
-    print()
-    count_by_symbol = input_pieces_count(row_count, col_count)
-
-    return row_count, col_count, count_by_symbol
-
-
-def mark_board_under_attack_cells(board, row_count, col_count, symbol='.'):
-    """
-    fill the empty cells that are under attack by other cells,
-    with given symbol
-    return a new board dict
-    """
-    new_board = {}
-    for row_num in range(row_count):
-        for col_num in range(col_count):
-            try:
-                new_board[(row_num, col_num)] = board[(row_num, col_num)]
-            except KeyError:
-                if ChessPiece.pos_attacked_by_board(row_num, col_num, board):
-                    new_board[(row_num, col_num)] = symbol
-    return new_board
+from cmd_chess_util import input_problem
 
 
 def count_or_show_by_generator(gen, count_enable, row_count, col_count):
@@ -181,21 +126,6 @@ def argparse_main():
     )
 
 # ______________________ Test Functions ______________________ #
-
-
-def test_input_pieces_count(row_count, col_count):
-    """test `input_pieces_count` function"""
-    count_by_symbol = input_pieces_count(row_count, col_count)
-    assert set(count_by_symbol.keys()) == \
-        set(ChessPiece.class_by_symbol.keys())
-    for count in count_by_symbol.values():
-        assert isinstance(count, int)
-        assert count >= 0
-
-    assert sum(count_by_symbol.values()) <= row_count * col_count
-
-    for symbol, count in count_by_symbol.items():
-        print('%s: %s' % (symbol, count))
 
 
 def compare_find_solutions_result():
